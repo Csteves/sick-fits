a56b72e4463a83b2daa8cdaff8f3869668451b25
Craig Stevens <csteves44@gmail.com>
2019-03-26 22:29:59 -0600

    Add Cart

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -14,10 +14,10 @@ const DELETE_ITEM_MUTATION = gql`
 
 class DeleteItem extends Component {
     update = (cache, payload) => {
-        //manually updat the cache on the client, so it matches the server
+        //manually update the cache on the client, so it matches the server
         // Read the cache form the items we want
         const data = cache.readQuery({query:ALL_ITEMS_QUERY});
-        d //filter out the deleted item
+         //filter out the deleted item
         data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
         //update the items in the cache with current data
         cache.writeQuery({query: ALL_ITEMS_QUERY, data});
@@ -33,7 +33,10 @@ class DeleteItem extends Component {
                     <button
                     onClick={() => {
                         if(confirm('Are you sure you want to delete this item?')){
-                            deleteItem()
+                            deleteItem().catch(err => {
+                                console.log(err)
+                                alert(err.message)
+                            })
                         }
                     }}
                     >{this.props.children}</button>
