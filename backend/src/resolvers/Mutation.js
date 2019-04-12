7e85159911e230f334f7208f892798392065d005
Craig Stevens <csteves44@gmail.com>
2019-04-11 22:05:33 -0600

    Add to cart type and resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -192,6 +192,43 @@ const mutations = {
         info
         );
         return updatedUser;
+    },
+    async addToCart(parent,args,ctx,info){
+        //1. Make sure they are signed in
+        const {userId} = ctx.request;
+        if(!userId){
+            throw new Error('You must be signed in sooon!');
+        };
+        //2. Query the users current cart
+        const [exsitingCartItem] = await ctx.db.query.cartItems({
+            where:{
+                user: {id: userId},
+                item: {id: args.id}
+            }
+        })
+        //3. Check ig that item is already in their cart and increment by 1 if it is
+        if(exsitingCartItem){
+            console.log("Item is already in cart")
+            return ctx.db.mutation.updateCartItem({
+                where:{
+                    id: exsitingCartItem.id,
+                },
+                data:{
+                    quantity: exsitingCartItem.quantity + 1,
+                },
+            },info)
+        }
+        //4. If its not, create a fresh CartItem that user
+        return ctx.db.mutation.createCartItem({
+            data:{
+                user:{
+                    connect: {id:userId}
+                },
+                item:{
+                    connect:{id:args.id}
+                }
+            }
+        },info)
     }
 };
 
